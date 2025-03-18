f967af4968c8dfe1ccd66a053e06383002f1c825
harleen001 <harleen.johal31@gmail.com>
2025-03-18 22:00:30 +0530

    variables

--- javascript/1.js
+++ javascript/1.js
@@ -1,1 +1,17 @@
 //starting with javascript
+
+//const PIE -> throughout code remains same
+//let
+//var  ->  can vary /variable
+
+const a=10
+a=20 //ERROR
+//CONSTANT CANNOT BE RENAMED BUT A VARIABLE CAN BE
+console.log(a)
+
+var b =10
+b=20; //NO ERROR
+console.log(b)
+
+
+//let vs var 
