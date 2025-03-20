0c553a29456357695667a9949cfc0db077833297
Kartik Attri <attrikartik494@gmail.com>
2025-03-20 09:55:46 +0530

    operation

--- javascript/2.js
+++ javascript/2.js
@@ -6,7 +6,40 @@
 
 // console.log(typeof (number_score))
 
-let value = 3 ;
-let negValue = -value ;
+//-------------------
 
-console.log(negValue)
+// let value = 3 ;
+// let negValue = -value ;
+
+// console.log(negValue);
+
+
+//------------------
+
+// let str1= "hello";
+// let str2= " harry bhau ";
+
+// let str3 = str1 + str2 ;
+
+// console.log(str3) ;
+
+//--------------------
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2 ) ;
+// console.log(1 + 2 + "2" ) ;
+
+
+// console.log((3 + 4) * 5 % 3);
+
+//------------------------
+
+// console.log(+true);
+// console.log(true);
+//console.log(true+); it will give error
+
+console.log(+"");
+
+//------------------------
