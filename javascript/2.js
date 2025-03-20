67c3b4eb61ea14889f27d513e19d21c73e5fb5b3
Kartik Attri <attrikartik494@gmail.com>
2025-03-20 10:12:25 +0530

    pre and post story

--- javascript/2.js
+++ javascript/2.js
@@ -37,13 +37,40 @@
 //------------------------
 
 // console.log(+true);--->1
-console.log(-true)
+
+// console.log(-true)
+
 // console.log(true);
 //console.log(true+); it will give error
 
 // console.log(+"");
 
-// //------------------------
+// ------------------------
 // 1--> true  0-->false
 // "harleen"--> true   ""-->false-->0
+
+//---------------------------
+
+// let gameCounter =100 ;
+// gameCounter ++ ;
+// console.log(gameCounter) ; 
+
+//-------------------------
+
+//  let gameCounter =100 ;
+//  ++gameCounter ;
+//  console.log(gameCounter) ; 
+ 
+
+//---------------------------- pre and post
+
+let x = 3;
+const y = x++;
+
+console.log(x,y);
+
+
+let a = 3;
+const b = ++a;
+console.log(a,b);
 
