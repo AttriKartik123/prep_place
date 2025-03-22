644d40746f182c86ef321d63b67496c5139e736c
harleen001 <harleen.johal31@gmail.com>
2025-03-22 22:52:48 +0530

    spread

--- javascript/4_Array.js
+++ javascript/4_Array.js
@@ -1,42 +1,66 @@
-const myArray=[0,1,2,3,4,5]
-// console.log(myArray[0]);
-const myArray2=new Array(0,3,4,5)//normal round bracket here
-myArray.push(7)//will add 7 to array
-console.log(myArray);
+// const myArray=[0,1,2,3,4,5]
+// // console.log(myArray[0]);
+// const myArray2=new Array(0,3,4,5)//normal round bracket here
+// myArray.push(7)//will add 7 to array
+// console.log(myArray);
 
-myArray.pop()//will remove from array
-console.log(myArray);
+// myArray.pop()//will remove from array
+// console.log(myArray);
 
-myArray.unshift(12)//aage add ho jayega
-console.log(myArray);
-myArray.shift()
-console.log(myArray);
-// 0 1 2 3 4 5 
-// 0 1 2 3 4 5 7
-// 0 1 2 3 4 5 
+// myArray.unshift(12)//aage add ho jayega
+// console.log(myArray);
+// myArray.shift()
+// console.log(myArray);
+// // 0 1 2 3 4 5 
+// // 0 1 2 3 4 5 7
+// // 0 1 2 3 4 5 
 
-// 12 0 1 2 3 4 5
+// // 12 0 1 2 3 4 5
 
-// push(5)  pop()----->pishe at the BACK
-// unshift(5)  shift()--> aage at FRONT
+// // push(5)  pop()----->pishe at the BACK
+// // unshift(5)  shift()--> aage at FRONT
 
-console.log(myArray.includes(9));//if present or not
-console.log(myArray.indexOf(4));
-
-
-const newArr=myArray.join()
-console.log(myArray);//array
-console.log(newArr)//string
-console.log(typeof newArr);
-
-console.log(myArray);//012345
-const splithere=myArray.slice(1,3)
-console.log(splithere);//12
-
-// splice()//also includes last element
-// 1 2 3//splices out of original array
-
-// 0 4 5
+// console.log(myArray.includes(9));//if present or not
+// console.log(myArray.indexOf(4));
 
 
+// const newArr=myArray.join()
+// console.log(myArray);//array
+// console.log(newArr)//string
+// console.log(typeof newArr);
 
+// console.log(myArray);//012345
+// const splithere=myArray.slice(1,3)
+// console.log(splithere);//12
+
+// // splice()//also includes last element
+// // 1 2 3//splices out of original array
+
+// // 0 4 5
+
+// // ----------2ND SHIFT----------------
+
+const marvel_heros=["thor","ironman","iman"]
+const dc_heros=["superman","doraemon","shizuka"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+//instead of adding two array it puts one array into another
+
+// console.log(marvel_heros[0])
+// console.log(marvel_heros[1])
+// console.log(marvel_heros[2])
+// console.log(marvel_heros[3])
+// console.log(marvel_heros[3][0])
+
+
+//concat doesnt change original array
+const abc=marvel_heros.concat(dc_heros)
+console.log(abc);
+console.log(marvel_heros)
+
+
+//spread 
+const new_heros=[...marvel_heros,...dc_heros]
+console.log(new_heros);
+...->>>>>>>>>spread
