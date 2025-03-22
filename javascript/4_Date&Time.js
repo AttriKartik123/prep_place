573866e80411758afdf457813ca4f0ba68b3ef88
harleen001 <harleen.johal31@gmail.com>
2025-03-22 12:12:27 +0530

    date

---
+++ javascript/4_Date&Time.js
@@ -0,0 +1,7 @@
+let myDate=new Date()
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
