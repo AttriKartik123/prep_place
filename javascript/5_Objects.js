9c7b7ad9973ad08ac5e999b6fb0094e0927d226d
Kartik Attri <attrikartik494@gmail.com>
2025-03-23 14:17:39 +0530

    Object started

---
+++ javascript/5_Objects.js
@@ -0,0 +1,40 @@
+//--------------Singleton: jab hm literals ki tarah declare krte hain tabb single tonn nhi bnegaa 
+
+//------------------------Object Literals:
+
+//Object.create // isme singleton bnega
+
+
+//----------------Symbol
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name:"Hitesh"  ,// name by default as a string lega
+    "fullname" : "Hitesh Choudhary" , // abb isse dot se access nhi kar skte
+    age: 18,  //key:value
+    mySym : "myKey1" ,
+    location: "Ghaamandi" ,
+    email : "annonymousannony@gmail.com",
+    isLoggedIn : false ,
+    LastloginDays : ["Monday" , "Saturday"] ,
+
+}
+//--accessing values
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["fullname"]);
+
+
+console.log(JsUser.mySym); // to print symbol -------- but agr abhi DT dekhenge toh iski DT symbol nhi hai , balki string dega
+console.log(typeof JsUser.mySym);
+
+console.log(JsUser["mySym"]);   //--------SYMBOL K LIYE ISSE SQUARE BRACKETS ME HI LIKHNA HOTA HAI 
+console.log(typeof JsUser.mySym);
+
+// console.log(JsUser.fullname); nhi dega output
+
+
+
+
+
