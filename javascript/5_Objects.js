c4d39fe792b43539fc5368d0eb29a10a1865ebf2
Kartik Attri <attrikartik494@gmail.com>
2025-03-23 14:41:03 +0530

    objects 1 done

--- javascript/5_Objects.js
+++ javascript/5_Objects.js
@@ -13,7 +13,7 @@ const JsUser = {
     name:"Hitesh"  ,// name by default as a string lega
     "fullname" : "Hitesh Choudhary" , // abb isse dot se access nhi kar skte
     age: 18,  //key:value
-    [mySym] : "myKey1" , //-------symbol k naam ko square bracket me likho , tabhi wo as a symbol kaam krega 
+    [mySym] : "myKey1" , //-------symbol k naam ko square bracket me likho , tabhi wo as a symbol kaam krega ... ///////------- wese agr hm mysym ki type pta krenge toh yeh undefined dega but agr hum [mysym] ki type krenge pta tb ye hmmme baaki elements ki tarah string dega
     location: "Ghaamandi" ,
     email : "annonymousannony@gmail.com",
     isLoggedIn : false ,
@@ -26,13 +26,33 @@ const JsUser = {
 // console.log(JsUser["fullname"]);
 
 
-console.log(JsUser[mySym]); // to print symbol -------- but agr abhi DT dekhenge toh iski DT symbol nhi hai , balki string dega
-console.log(typeof JsUser[mySym]);
+// console.log(JsUser[mySym]); // to print symbol -------- but agr abhi DT dekhenge toh iski DT symbol nhi hai , balki string dega
+// console.log(typeof JsUser[mySym]);
 
-console.log(JsUser[mySym]);   //--------SYMBOL K LIYE ISSE SQUARE BRACKETS ME HI LIKHNA HOTA HAI 
+// console.log(JsUser[mySym]);   //--------SYMBOL K LIYE ISSE SQUARE BRACKETS ME HI LIKHNA HOTA HAI 
 
-// console.log(JsUser.fullname); nhi dega output
+//----changing value: 
+JsUser.email= "harleenjohaolchatgpt.com";
 
+//-----object freeze - no change in value
+// Object.freeze(JsUser);
+
+// console.log(JsUser);
+
+
+//Functions
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name} `);
+}
+
+// console.log(JsUser.greeting); // it will give undefined -----op--->[Function (anonymous)]
+ console.log(JsUser.greeting());
+ console.log(JsUser.greetingTwo()); 
 
 
 
