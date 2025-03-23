5efbbad85e95480eb5d6cdd1fb579ef95fc8619a
Kartik Attri <attrikartik494@gmail.com>
2025-03-24 00:06:11 +0530

    question

--- javascript/5_Function.js
+++ javascript/5_Function.js
@@ -31,3 +31,21 @@ console.log(loginusermessage("harleen"));
 console.log(loginusermessage("kartik"));
 //when no parameter is passed the value is
 //undefined
+
+
+
+//-----> question given by harleen sir
+function bodmas(a, b) {
+    let result = a + b;
+    console.log(result);
+
+    let result1 = a - b;
+    console.log(result1);
+
+    let result2 = a / b;
+    console.log(result2);
+
+    return result;
+}
+
+bodmas(20, 30);
