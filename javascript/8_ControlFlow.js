8713110ebc9eaf9c7e528ad2462485bc79075604
Kartik Attri <attrikartik494@gmail.com>
2025-03-30 13:38:18 +0530

    switch question given by harry nbhau

--- javascript/8_ControlFlow.js
+++ javascript/8_ControlFlow.js
@@ -114,3 +114,39 @@ gradetocgpa("X");
 // 6
 // 7->sunday
 // any other value->invalid day
+
+function getDayName(dayNumber) {
+    let daynumber = 1
+    switch (dayNumber) {
+      case 1:
+        console.log("monday");
+        break;
+      case 2:
+        console.log("tuesday");
+        break;
+      case 3:
+        console.log("wednesday");
+        break;
+      case 4:
+        console.log("thursday");
+        break;
+      case 5:
+        console.log("friday");
+        break;
+      case 6:
+        console.log("saturday");
+        break;
+      case 7:
+        console.log("sunday");
+        break;
+
+      default:
+        return "Invalid day";
+       
+    }
+  }
+  
+  
+  console.log(getDayName(3)); 
+  
+  
