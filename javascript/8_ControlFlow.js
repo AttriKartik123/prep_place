a69a5b4799cff1086b507cabd24eda12696d6d32
Kartik Attri <attrikartik494@gmail.com>
2025-03-30 13:17:42 +0530

    Control flows

---
+++ javascript/8_ControlFlow.js
@@ -0,0 +1,52 @@
+// NODE EK ENGINE HAI JO JS KO EXECUTE KRTI HAI // 
+//control flow
+
+//if ---->
+
+// if (condition) {
+// // consition should be true
+// }
+
+// const isUserloggedIn = true ;
+
+// if (isUserloggedIn){
+
+// }
+// if(2 == '2'){
+//     console.log("Executed");
+// }
+
+// compaison operator ------>   <,>,<=,==,!= , ===(type bhi check krega), 
+
+
+
+
+
+// const score = 200 ;
+
+// if(score > 100) {
+//     const power = " fly" ;
+//     console.log(`user power : ${power}`);
+// }
+
+// console.log(`user power : ${power}`)  // op nhi aaigi
+
+
+
+// const score = 200 ;
+
+// if(score > 100) {
+//     var power = " fly" ;
+//     console.log(`user power : ${power}`); // op aaigi kyuki var use kiya hai 
+// }
+
+// console.log(`user power : ${power}`)  // op aaigi kyuki var use kiya hai 
+
+
+//Short hand notation :
+const balance = 1000 
+if(balance > 500) console.log("test"); //implicit scope
+
+
+
+
