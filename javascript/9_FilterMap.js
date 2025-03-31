1b1e9d3cc04cfc1750afb039c7f11cd775a11528
Kartik Attri <attrikartik494@gmail.com>
2025-03-31 10:54:15 +0530

    9 day

---
+++ javascript/9_FilterMap.js
@@ -0,0 +1,9 @@
+const coding = ["js" , "ruby" , "java" , "python"] ;
+
+const values = coding.forEach (item) => { // you have to manual return a value in foreach  function
+    //console.log(item);
+    return item ;
+    
+});
+
+console.log(values);
