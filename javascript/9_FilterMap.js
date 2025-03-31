7ac0a2edfe71d9b688b5e8cafcb03e715dc86f79
Kartik Attri <attrikartik494@gmail.com>
2025-03-31 11:12:03 +0530

    filter usage

--- javascript/9_FilterMap.js
+++ javascript/9_FilterMap.js
@@ -1,9 +1,77 @@
-const coding = ["js" , "ruby" , "java" , "python"] ;
+// const coding = ["js" , "ruby" , "java" , "python"] ;
 
-const values = coding.forEach (item) => { // you have to manual return a value in foreach  function
-    //console.log(item);
-    return item ;
+// const values = coding.forEach( (item)=> { // you have to manual return a value in foreach  function
+//     //console.log(item.name);
+//     return item ;
     
-});
+// }); // undefined return krega 
 
-console.log(values);
+// console.log(values);
+
+
+
+
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10] ;
+// // filter operation
+// // myNums.filter((num) => num > 4  ) // takes callback 
+// // returns the values but foreach doesnot
+// // {}= scope = mtlb khud se return krna pdega  , ()= return
+// const newNums = myNums.filter((num) => {
+//     return num > 4;
+//  });
+ 
+//  console.log(newNums);
+ 
+
+
+
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNums = [];
+
+// myNums.forEach((num) => {
+//     if (num > 4) {
+//         newNums.push(num); // push into newNums if condition is true
+//     }
+// });
+
+// console.log(newNums);
+
+// //-----> // foreach naal kuch v return nhi ho reha , jdo filter la
+
+// // foreach se return ki hai vaue abb , newNums ek array bnaali and myNum se waha pe push krdegataa pher value return hogi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition:1989},
+];
+// wo books jinka genre History hai wo niokaalo 
+const userBooks = books.filter((bk) => bk.genre === 'History')
+console.log(userBooks);
