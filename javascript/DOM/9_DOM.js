28c19b80c4549604f117d2365d31cf9b1a143aa3
harleen001 <harleen.johal31@gmail.com>
2025-03-31 15:45:56 +0530

    dom

---
+++ javascript/DOM/9_DOM.js
@@ -0,0 +1,24 @@
+const title=document.getElementById('title')
+undefined
+title.style.backgroundColor='yellow'
+'yellow'
+title
+    title.textContent
+'DOM Learning on chai aur code '
+title.innerHTML
+'DOM Learning on chai aur code '
+title.innerText
+'DOM Learning on chai aur code'
+
+title.innerText-->//only shows visible text
+
+title.textContent-->//shows only text but also non-visible text
+
+title.innerHTML//shows whole html
+'DOM Learning on chai aur code\n<span style="display: none;">test text</span> '
+
+
+document.querySelector('h1')//selects only first
+document.querySelectorAll('h1')//selects all
+
+
