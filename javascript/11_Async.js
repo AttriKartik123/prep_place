935aafc892b2a885ad18edf55701215587a06f29
Kartik Attri <attrikartik494@gmail.com>
2025-04-02 22:27:54 +0530

    Async Intro

---
+++ javascript/11_Async.js
@@ -0,0 +1,24 @@
+// javascript 
+//-> Synchronous (By default)-- line me chlega pehle uper se then neeche wala chlega
+//-> Single threaded
+
+//Execution control
+//-> execute ine line of code at a tym 
+//-> console log ->1
+//-> console log ->2            each operation waits for the lastt one to complete before executing
+//-> CALL Stack and Memory Stack
+
+
+// Blocking vs non Blocking Code 
+
+// Blocking- block the flow of the program -> Read file sync
+
+// Non_Blocking Code => doesnot block execution -> Read File Async -> Async mtlb baaki ke kaam kro mai file ko read krke aaya jra
+
+// set timeout - given tym k baad execute ho ,  and set time interval 
+
+// queue - mcD line
+// stack - LIFO
+
+// Fetch API - High priority 
+
