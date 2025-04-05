4834768800760e3465e0289c988ddc5dbc7ce9e9
Kartik Attri <attrikartik494@gmail.com>
2025-04-05 17:46:46 +0530

    Fetch

---
+++ javascript/13_Fetch.js
@@ -0,0 +1,28 @@
+//  fetch API pehle browser native API thi isiliye isse add nhi kiya gya tha node js me  
+//  abb add isiliye krdiya gya taakki ek compatibility and consitency mil skti browser and local k liye-->
+
+//  <!-- status code - IRSC
+//  1xx: informational
+//  2xx: success
+//  3xx: maintainence and redirectional
+//  4xx: client errors
+
+//  MicroTask queue(Call Back queue) ---- fetch queue (fetch alag se apni queue bnata hai)
+//  Fetch= VIP Line (Mtlb pehle iska kaam hoga)
+ 
+
+
+
+// FETCH WORKING FULL: -----------------------
+
+//  response =  fetch ('Something')  // Response is a global variable
+//              |   | 
+//           Data:  WebBrowser\node
+//           On fullfilled[]    |
+//           On Rejecction[]    Network Request milegi 
+//                              |        |
+//                              Yes      No
+//                              |        |
+//                     Onfullfilled     On Rejection
+//                     pe chlajaiga        pe chlega
+ 
