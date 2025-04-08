f3ad1ace095b7bd5076b0cafe6ab875a1c0e38e0
harleen001 <harleen.johal31@gmail.com>
2025-04-08 18:08:07 +0530

    gitpush

---
+++ javascript/16_callandthis.js
@@ -0,0 +1,16 @@
+function setusername(username)
+{
+    //if browser then browser window
+    //if node then an empty object it will refer to
+    this.username=username
+}
+
+function createuser(username,email,password)
+{
+    setusername.call(this,username)//not called yet
+    this.email=email
+    this.password=password
+
+}
+const chai=new createuser("chai","chai@fb.com","123")
+console.log(chai);
