294f69b41af04a4f526c26540c6aecf0eece9770
Kartik Attri <attrikartik494@gmail.com>
2025-04-08 18:42:57 +0530

    Class constructor

---
+++ javascript/16_ClassConstructor.js
@@ -0,0 +1,55 @@
+// class User {
+//     constructor(username, email, password){
+//         this.username= username ;
+//         this.email=email;
+//         this.password= password;  // prototype ban gya 
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+// }
+//     const chai = new User("chai", "chai@gmail.com", "123") ;
+//     console.log(chai);
+//     console.log(chai.encryptPassword());
+
+    
+
+
+
+
+//INHERITANCE
+class User{
+    constructor(username){
+        this.username = username 
+    }
+
+    logMe(){
+        console.log(
+            `USERNAME is ${this.username}`);
+        
+    }
+}
+
+
+class Teacher extends User {
+    constructor(username , email ,password){
+        super(username) // Highest Class 
+        this.email=email 
+        this.password=password
+    }
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+
+// const chai= Teacher("chai", "chai@gmail.com" , "123"); // new aaiga yaha pe
+// const chai2= Teacher("chai", "chai@gmail.com" , "123"); // new aaiga yaha pe
+
+
+const chai=new Teacher("chai", "chai@gmail.com" , "123"); 
+const Newchai=new Teacher("Tea", "chai@gmail.com" , "123"); 
+
+chai.addCourse();
+Newchai.addCourse();
