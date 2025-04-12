824a483b734046d48e920ad513d1572c5f09ae62
Kartik Attri <attrikartik494@gmail.com>
2025-04-12 14:29:54 +0530

    React folder structure understanding 1

---
+++ react/01_basic/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Chai from './Chai'
+function App() {
+ 
+
+  return (
+    <>
+   <h3>bjb</h3>
+     <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ react/01_basic/src/Chai.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function Chai() {
+    return(
+        <>
+        <h3>Chai Is ready</h3>
+        <h3>Chai was ready</h3>
+        <h3>Chai will be ready</h3>
+        </>
+    )
+}
+
+export default Chai
