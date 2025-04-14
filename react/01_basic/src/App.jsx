4a09cf3123da6dd0276e3e23243cd674b0af5e99
Kartik Attri <attrikartik494@gmail.com>
2025-04-14 16:50:46 +0530

    React 02_ Project to incrememt and decrememt

--- react/01_basic/src/App.jsx
+++ react/01_basic/src/App.jsx
@@ -1,15 +1,51 @@
-import { useState } from 'react'
+import { useState } from 'react' 
 import Chai from './Chai'
-function App() {
- 
 
+function App() {
+  // state ko change krdega - DOM ke andar change ko propagate kiya jata hai 
+ let [counter, setCounter] =useState(15)// parenthesis k andar default value hogi
+
+
+
+// let counter = 15
+const addValue = () => {
+  console.log("Value added :" , Math.random());
+  console.log("Clicked" , counter);
+  if(counter<20)
+  {counter = counter +1 
+  setCounter(counter)}
+
+  else{console.log("Value is greater than 20");
+    }
+ 
+}
+const removeValue = () => {
+  console.log("Value added :" , Math.random());
+  console.log("Clicked" , counter) ;
+  if(counter>0)
+  {
+  counter = counter - 1  
+  setCounter(counter)
+}
+else{console.log("sasrikaaal jiiii");
+}
+}
   return (
   //   <>
   //  <h3>bjb</h3>
   //    <Chai/>
 
   <>
-  
+  <h1>Chai Aur react</h1>
+  <h2>Counter Value : {counter}</h2> 
+
+   {/* HOOKS USED TO UPDATE DATA ON UI in react  */}
+
+  <button onClick={addValue}>Add Value {counter}</button>
+  <br />
+  <button onClick = {removeValue}>Remove Value{counter}</button>
+
+
   </>
   )
 }
