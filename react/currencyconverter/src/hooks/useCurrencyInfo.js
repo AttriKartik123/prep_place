aa65e15c6d7eff2c5ada2b220e3347f4ee468286
harleen001 <harleen.johal31@gmail.com>
2025-06-14 14:21:14 +0530

    currency-converter

--- react/currencyconverter/src/hooks/useCurrencyInfo.js
+++ react/currencyconverter/src/hooks/useCurrencyInfo.js
@@ -7,13 +7,13 @@ function useCurrencyInfo(currency) // currency is a variable
     //useeffect hook - callback and dependency  
     const [data , setData] = useState({})
     useEffect(() => {
-    fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.6.13/v1/currencies/${currency}.json')
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.6.13/v1/currencies/${currency}.json`)
     .then((res) => res.json()) // object ki structuring 
     .then((res) => setData(res[currency])) //.then- used to handle the outut of an API 
-    .console.log(data);
-    }, [currency]) // taaki jb bhi currency me change ho tabhi ye dobara se chle
+    console.log(data);
+    }, [currency, data]) // taaki jb bhi currency me change ho tabhi ye dobara se chle
     
-    .console.log(data);
+    console.log(data);
     return data 
 
 }
