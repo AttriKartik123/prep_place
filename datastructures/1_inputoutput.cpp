cea86ceb457b245639d5a6a3c01ac5d286901361
harleen001 <harleen.johal31@gmail.com>
2025-06-17 22:49:20 +0530

    i/o

---
+++ datastructures/1_inputoutput.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a; //taking input
+    cout<<a; //producing ouput
+    return 0;//if not returned it will give a garbage value from memory
+}
