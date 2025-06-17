06744f54ae8d9f09c53dfee920157451a69634ad
harleen001 <harleen.johal31@gmail.com>
2025-06-17 23:02:50 +0530

    age

---
+++ datastructures/1_ifelse.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int age=12;
+if(age>=18)
+{
+    cout<<"Child is adult now";
+}
+else
+{
+cout<<"Child is still not adult";
+}
+
+    return 0;
+}
