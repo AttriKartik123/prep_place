b7a9df9753bbd98beb673595781ad23f0ef0f67a
Kartik Attri <attrikartik494@gmail.com>
2025-06-18 00:38:31 +0530

    array

--- datastructures/1_array-strings.cpp
+++ datastructures/1_array-strings.cpp
@@ -12,7 +12,29 @@ int main()
     {
         cout<<arr1[i];
     }
-    int arr3[10];
+    // int arr3[10];
+
+    // for (int i=0 ; i<10 ; i++){
+    //     cin>>arr3[i] ;
+
+    // }
+    // for (int i=0 ; i<10 ; i++){
+    //     cout<<arr3[i] <<endl;
+    // }
+
+    /////////////////////STRING/////////////////////////
+    string s=" kartik";
+    int stringsize=s.size();
+    int stringlength=s.length();//length and size remains same, count spaces also
+    int stringcapacity=s.capacity();
+    cout<<stringsize<<stringlength<<stringcapacity;//depends on compiler
     
+    cout<<s.substr(1,4);
+    
+    for(int i=0;i<stringsize;i++)
+    {
+        cout<<s[i]<<endl;
+    }
+
     return 0;
 }
