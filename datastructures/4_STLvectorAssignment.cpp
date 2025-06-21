e93e164cbbb7d404cb946e6aae1230324ad55584
Kartik Attri <attrikartik494@gmail.com>
2025-06-21 12:42:15 +0530

    Vector Assignment

---
+++ datastructures/4_STLvectorAssignment.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of students :";
+
+    cin >> n;
+
+    vector<int> percentages(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int percent;
+        cout << "Enter percentage :";
+        cin >> percentages[i];
+        percentages.insert(percentages.end(), percent);
+    }
+
+    if (n == 2)
+    {
+        percentages[1] = 50;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << percentages[i] << endl;
+    }
+
+    return 0;
+}
